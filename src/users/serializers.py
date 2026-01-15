dbdcc5c1bc5802b2da1c89e6575604fd41fbf746
unknown <obamaluc36@gmail.com>
2026-01-15 11:12:58 +0100

    Mise a Jour

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,12 +6,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "password"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data.get("email"),
             password=validated_data["password"],
         )
         return user
